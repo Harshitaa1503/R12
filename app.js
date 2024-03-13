49ac2d0284bf1d44da2f4ff3c3749ca5f1395133
Harshitaa1503 <malviyaharshita1503@gmail.com>
2024-03-13 17:29:51 +0530

    R12

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ require("dotenv").config({path: "./.env"});
 const express = require("express");
 const app = express();
 
+require('./models/dbconfig').dbconnection()
+
 // const dotenv = require("dotenv");
 // dotenv.config({path: "./.env"});
 
-const PORT= process.env.PORT;
-
+// const PORT= process.env.PORT;
+//mongodb+srv://harshita:<password>@cluster0.uolns2k.mongodb.net/?retryWrites=true&w=majority
 
 
 app.listen(process.env.PORT,()=>{
