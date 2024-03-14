91c1b51a0cd1112cdd1416c4e5ff0a213f2fa38e
Harshitaa1503 <malviyaharshita1503@gmail.com>
2024-03-14 21:13:42 +0530

    setup of logger &  body parser

--- app.js
+++ app.js
@@ -5,16 +5,32 @@ const app = express();
 
 require('./models/dbconfig').dbconnection()
 
+//routes
+const userRouter= require("./routes/userRoute")
+
+//logger
+const logger = require("morgan")
+app.use(logger("tiny"))
+//body parser
+
+app.use(express.json)
+app.use(express.urlencoded({extended:true}))
+
+
+//http://localhost:8080/api/user/
+//http://localhost:8080/api/user/create
+
+app.use("/api/user", userRouter);
+
+//server
+app.listen(process.env.PORT,()=>{
+    console.log (`the server is running on port ${process.env.PORT}`)
+})
+
+
 // const dotenv = require("dotenv");
 // dotenv.config({path: "./.env"});
 
 // const PORT= process.env.PORT;
 //mongodb+srv://harshita:<password>@cluster0.uolns2k.mongodb.net/?retryWrites=true&w=majority
 
-
-app.listen(process.env.PORT,()=>{
-    console.log (`the server is running on port ${process.env.PORT}`)
-})
-
-// git config --global user.email "you@example.com"
-// git config --global user.name "Your Name" run in terminal
